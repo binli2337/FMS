fbeb7f622f92a74add27c62fa51b460fddfe7556
Bin.Li <bin.li@noaa.gov>
2021-06-29 16:44:51 +0000

    update files in coupler directory.

--- coupler/atmos_ocean_fluxes.F90
+++ coupler/atmos_ocean_fluxes.F90
@@ -36,10 +36,12 @@
 !! the individual fields, or fluxes. This could be used in
 !! the absence of an atmospheric or oceanic model.
 module  atmos_ocean_fluxes_mod
+#include <fms_platform.h>
   use mpp_mod,           only: stdout, mpp_error, FATAL, mpp_sum, mpp_npes
   use fms_mod,           only: write_version_number
 
   use coupler_types_mod, only: coupler_1d_bc_type
+  use coupler_types_mod, only: coupler_1d_bc_type_r8
   use coupler_types_mod, only: ind_alpha, ind_csurf, ind_sc_no
   use coupler_types_mod, only: ind_pcair, ind_u10, ind_psurf
   use coupler_types_mod, only: ind_deposition
@@ -70,7 +72,11 @@ module  atmos_ocean_fluxes_mod
 
   character(len=*), parameter :: mod_name = 'atmos_ocean_fluxes_mod'
   real, parameter :: epsln=1.0e-30
-
+ 
+  interface  aof_set_coupler_flux
+  module procedure  aof_set_coupler_flux_r
+  module procedure  aof_set_coupler_flux_r8
+  end interface aof_set_coupler_flux
 
   ! Include variable "version" to be written to log file.
 #include<file_version.h>
@@ -102,7 +108,7 @@ contains
   !!     Mismatch between parameter input and the parameters being replaced
   !! \throw FATAL, "Could not change back to [current_list]"
   !! \throw FATAL, "Empty [name] list"
-  function aof_set_coupler_flux(name, flux_type, implementation, atm_tr_index, param, flag,&
+  function aof_set_coupler_flux_r(name, flux_type, implementation, atm_tr_index, param, flag,&
       & mol_wt, ice_restart_file, ocean_restart_file, units, caller, verbosity) &
       & result (coupler_index)
     character(len=*), intent(in)                :: name !< name
@@ -388,7 +394,295 @@ contains
     endif
 
     return
-  end function aof_set_coupler_flux
+  end function aof_set_coupler_flux_r
+
+  function aof_set_coupler_flux_r8(name, flux_type, implementation, atm_tr_index, param, flag,&
+      & mol_wt, ice_restart_file, ocean_restart_file, units, caller, verbosity) &
+      & result (coupler_index)
+    character(len=*), intent(in)                :: name !< name
+    character(len=*), intent(in)                :: flux_type !< flux_type
+    character(len=*), intent(in)                :: implementation !< implementation
+    integer, intent(in), optional               :: atm_tr_index !< atm_tr_index
+    real(DOUBLE_KIND), intent(in), dimension(:), optional    :: param !< param
+    logical, intent(in), dimension(:), optional :: flag !< flag
+    real(DOUBLE_KIND), intent(in), optional     :: mol_wt !< mol_wt
+    character(len=*), intent(in), optional      :: ice_restart_file !< ice_restart_file
+    character(len=*), intent(in), optional      :: ocean_restart_file !< ocean_restart_file
+    character(len=*), intent(in), optional      :: units !< units
+    character(len=*), intent(in), optional      :: caller !< caller
+    integer,          intent(in), optional      :: verbosity  !< A 0-9 integer indicating a level of verbosity.
+
+    integer :: coupler_index
+
+    character(len=*), parameter  :: sub_name = 'aof_set_coupler_flux_r8'
+
+    integer                                                 :: n
+    integer                                                 :: length
+    integer                                                 :: num_parameters
+    integer                                                 :: outunit
+    character(len=fm_path_name_len)                         :: coupler_list
+    character(len=fm_path_name_len)                         :: current_list
+    character(len=fm_string_len)                            :: flux_type_test
+    character(len=fm_string_len)                            :: implementation_test
+    character(len=256)                                      :: error_header
+    character(len=256)                                      :: warn_header
+    character(len=256)                                      :: note_header
+    character(len=128)                                      :: flux_list
+    character(len=128)                                      :: caller_str
+    character(len=fm_string_len), pointer, dimension(:)     :: good_list => NULL()
+    character(len=256)                                      :: long_err_msg
+    integer :: verbose ! An integer indicating the level of verbosity.
+
+    verbose = 5 ! Default verbosity level
+    if (present(verbosity)) verbose = verbosity
+
+    ! Set the caller string and headers.
+    if (present(caller)) then
+      caller_str = '[' // trim(caller) // ']'
+    else
+      caller_str = fm_util_default_caller
+    endif
+
+    error_header = '==>Error from ' // trim(mod_name) //&
+        & '(' // trim(sub_name) // ')' // trim(caller_str) // ':'
+    warn_header = '==>Warning from ' // trim(mod_name) //&
+        & '(' // trim(sub_name) // ')' // trim(caller_str) // ':'
+    note_header = '==>Note from ' // trim(mod_name) //&
+        & '(' // trim(sub_name) // ')' // trim(caller_str) // ':'
+
+    ! Check that a name is given (fatal if not).
+    if (name .eq. ' ') then
+      call mpp_error(FATAL, trim(error_header) // ' Empty name given')
+    endif
+    outunit = stdout()
+    if (verbose >= 5) then
+      write (outunit,*)
+      write (outunit,*) trim(note_header), ' Processing coupler fluxes ', trim(name)
+    endif
+
+    ! Define the coupler list name.
+    coupler_list = '/coupler_mod/fluxes/' // trim(name)
+
+    ! Check whether a flux has already been set for this name, and if so, return the index for it
+    ! (this is because the fluxes may be defined in both the atmosphere and ocean models) (check
+    ! whether the good_list list exists, since this will indicate that this routine has already been
+    ! called, and not just that the field table input has this list defined)
+    if (fm_exists('/coupler_mod/GOOD/fluxes/' // trim(name) // '/good_list')) then
+      if (verbose >= 5) then
+        write (outunit,*)
+        write (outunit,*) trim(note_header), ' Using previously defined coupler flux'
+      endif
+      coupler_index = fm_get_index(coupler_list)
+      if (coupler_index .le. 0) then
+        call mpp_error(FATAL, trim(error_header) // ' Could not get coupler flux ')
+      endif
+
+      ! Allow atm_tr_index to be set here, since it will only be set from atmospheric
+      ! PEs, and the atmospheric routines call this routine last, thus overwriting the
+      ! current value is safe (furthermore, this is not a value which could have any meaningful
+      ! value set from the run script.
+      if (present(atm_tr_index)) then
+        if (verbose >= 5) &
+            write (outunit,*) trim(note_header), ' Redefining atm_tr_index to ', atm_tr_index
+        call fm_util_set_value(trim(coupler_list) // '/atm_tr_index', atm_tr_index,&
+            & no_create = .true., no_overwrite = .false., caller = caller_str)
+      endif
+      return
+    endif
+
+    ! Set a new coupler flux and get its index.
+    coupler_index = fm_new_list(coupler_list)
+    if (coupler_index .le. 0) then
+      call mpp_error(FATAL, trim(error_header) // ' Could not set coupler flux ')
+    endif
+
+    ! Change to the new list, first saving the current list.
+    current_list = fm_get_current_list()
+    if (current_list .eq. ' ') then
+      call mpp_error(FATAL, trim(error_header) // ' Could not get the current list')
+    endif
+
+    if (.not. fm_change_list(coupler_list)) then
+      call mpp_error(FATAL, trim(error_header) // ' Could not change to the new list')
+    endif
+
+    ! Set the array in which to save the valid names for this list,
+    ! used later for a consistency check. This is used in the fm_util_set_value
+    ! routines to make the list of valid values.
+    call fm_util_set_good_name_list('/coupler_mod/GOOD/fluxes/' // trim(name) // '/good_list')
+
+    ! Set other defaults for the fm_util_set_value routines.
+    call fm_util_set_no_overwrite(.true.)
+    call fm_util_set_caller(caller_str)
+
+    ! Set various values to given values, or to defaults if not given.
+    if (flux_type .eq. ' ') then
+      call mpp_error(FATAL, trim(error_header) // ' Blank flux_type given')
+    else
+      if (fm_exists('/coupler_mod/types/' // trim(flux_type))) then
+        call fm_util_set_value('flux_type', flux_type)
+
+        ! Check that the flux_type that we will use (possibly given from the field_table)
+        ! is defined.
+        flux_type_test = fm_util_get_string('flux_type', scalar = .true.)
+        if (.not. fm_exists('/coupler_mod/types/' // trim(flux_type_test))) then
+          call mpp_error(FATAL, trim(error_header) //&
+              & ' Undefined flux_type given from field_table: ' // trim(flux_type_test))
+        endif
+      else
+        call mpp_error(FATAL, trim(error_header) //&
+            & ' Undefined flux_type given as argument to the subroutine: ' // trim(flux_type))
+      endif
+    endif
+
+    if (implementation .eq. ' ') then
+      call mpp_error(FATAL, trim(error_header) // ' Blank flux_type given')
+    else
+      if (fm_exists('/coupler_mod/types/' // trim(flux_type) // '/implementation/' // trim(implementation))) then
+        call fm_util_set_value('implementation', implementation)
+
+        ! Check that the flux_type/implementation that we will use
+        ! (both possibly given from the field_table) is defined
+        implementation_test = fm_util_get_string('implementation', scalar = .true.)
+        if (.not. fm_exists('/coupler_mod/types/' // trim(flux_type_test) //  '/implementation/' // trim(implementation_test))) then
+          if (flux_type .eq. flux_type_test) then
+            if (implementation .eq. implementation_test) then
+              call mpp_error(FATAL, trim(error_header) // ' Should not get here, as it is tested for above')
+            else
+              call mpp_error(FATAL, trim(error_header) //&
+                  & ' Undefined flux_type/implementation (implementation given from field_table): ' //&
+                  & trim(flux_type_test) // '/implementation/' // trim(implementation_test))
+            endif
+          else
+            if (implementation .eq. implementation_test) then
+              long_err_msg = 'Undefined flux_type/implementation (flux_type given from field_table): '
+              long_err_msg = long_err_msg // trim(flux_type_test) // '/implementation/'&
+                  & // trim(implementation_test)
+              call mpp_error(FATAL, trim(error_header) // long_err_msg)
+            else
+              long_err_msg = ' Undefined flux_type/implementation (both given from field_table): '
+              long_err_msg = long_err_msg //  trim(flux_type_test) // '/implementation/'&
+                  & // trim(implementation_test)
+              call mpp_error(FATAL, trim(error_header) // long_err_msg)
+            endif
+          endif
+        endif
+      else
+        call mpp_error(FATAL, trim(error_header) //&
+            & ' Undefined flux_type/implementation given as argument to the subroutine: ' //&
+            & trim(flux_type) // '/implementation/' // trim(implementation))
+      endif
+    endif
+
+    if (present(atm_tr_index)) then
+      call fm_util_set_value('atm_tr_index', atm_tr_index)
+    else
+      call fm_util_set_value('atm_tr_index', 0)
+    endif
+
+    if (present(mol_wt)) then
+      call fm_util_set_value('mol_wt', mol_wt)
+    else
+      call fm_util_set_value('mol_wt', 0.0)
+    endif
+
+    if (present(ice_restart_file)) then
+      call fm_util_set_value('ice_restart_file', ice_restart_file)
+    else
+      call fm_util_set_value('ice_restart_file', 'ice_coupler_fluxes.res.nc')
+    endif
+
+    if (present(ocean_restart_file)) then
+      call fm_util_set_value('ocean_restart_file', ocean_restart_file)
+    else
+      call fm_util_set_value('ocean_restart_file', 'ocean_coupler_fluxes.res.nc')
+    endif
+
+    if (present(param)) then
+      num_parameters = fm_util_get_integer('/coupler_mod/types/' //&
+          & trim(fm_util_get_string('flux_type', scalar = .true.)) // '/implementation/' //&
+          & trim(fm_util_get_string('implementation', scalar = .true.)) // '/num_parameters',&
+          & scalar = .true.)
+      length = min(size(param(:)),num_parameters)
+      if ((length .ne. num_parameters) .and. (verbose >= 5)) then
+        write (outunit,*) trim(note_header), ' Number of parameters provided for ', trim(name), ' does not match the'
+        write (outunit,*) 'number of parameters required (', size(param(:)), ' != ', num_parameters, ').'
+        write (outunit,*) 'This could be an error, or more likely is just a result of the implementation being'
+        write (outunit,*) 'overridden by the field table input'
+      endif
+      if (length .gt. 0) then
+        call fm_util_set_value('param', param(1:length), length)
+      else
+        call fm_util_set_value('param', 'null', index = 0)
+      endif
+    else
+      call fm_util_set_value('param', 'null', index = 0)
+    endif
+
+    if (present(flag)) then
+      call fm_util_set_value('flag', flag, size(flag(:)))
+    else
+      call fm_util_set_value('flag', .false., index = 0)
+    endif
+
+    flux_list = '/coupler_mod/types/' // trim(flux_type) // '/'
+
+    if (present(units)) then
+      call fm_util_set_value(trim(fm_util_get_string(trim(flux_list) // 'flux/name', index = ind_flux)) // '-units',&
+          & units)
+    else
+      call fm_util_set_value(trim(fm_util_get_string(trim(flux_list) // 'flux/name', index = ind_flux)) // '-units',&
+          & fm_util_get_string(trim(flux_list) // 'flux/units', index = ind_flux))
+    endif
+
+    do n = 1, fm_util_get_length(trim(flux_list) // 'flux/name')
+      if (n .ne. ind_flux) then
+        call fm_util_set_value(trim(fm_util_get_string(trim(flux_list) // 'flux/name', index = n)) // '-units',&
+            & fm_util_get_string(trim(flux_list) // 'flux/units', index = n))
+      endif
+      call fm_util_set_value(trim(fm_util_get_string(trim(flux_list) // 'flux/name', index = n)) // '-long_name',&
+          & fm_util_get_string(trim(flux_list) // 'flux/long_name', index = n))
+    enddo  ! n
+
+    do n = 1, fm_util_get_length(trim(flux_list) // 'atm/name')
+      call fm_util_set_value(trim(fm_util_get_string(trim(flux_list) // 'atm/name', index = n)) //&
+          & '-units', fm_util_get_string(trim(flux_list) // 'atm/units', index = n))
+      call fm_util_set_value(trim(fm_util_get_string(trim(flux_list) // 'atm/name', index = n)) // '-long_name',&
+          & fm_util_get_string(trim(flux_list) // 'atm/long_name', index = n))
+    enddo  ! n
+
+    do n = 1, fm_util_get_length(trim(flux_list) // 'ice/name')
+      call fm_util_set_value(trim(fm_util_get_string(trim(flux_list) // 'ice/name', index = n)) // '-units',&
+          & fm_util_get_string(trim(flux_list) // 'ice/units', index = n))
+      call fm_util_set_value(trim(fm_util_get_string(trim(flux_list) // 'ice/name', index = n)) // '-long_name',&
+          & fm_util_get_string(trim(flux_list) // 'ice/long_name', index = n))
+    enddo  ! n
+
+    ! Reset the defaults for the fm_util_set_value calls.
+    call fm_util_reset_good_name_list
+    call fm_util_reset_no_overwrite
+    call fm_util_reset_caller
+
+    ! Change back to the saved current list.
+    if (.not. fm_change_list(current_list)) then
+      call mpp_error(FATAL, trim(error_header) // ' Could not change back to ' // trim(current_list))
+    endif
+
+    !  Check for any errors in the number of fields in this list.
+    if (caller_str .eq. ' ') then
+      caller_str = trim(mod_name) // '(' // trim(sub_name) // ')'
+    endif
+    good_list => fm_util_get_string_array('/coupler_mod/GOOD/fluxes/' // trim(name) // '/good_list',&
+        & caller = caller_str)
+    if (associated(good_list)) then
+      call fm_util_check_for_bad_fields(trim(coupler_list), good_list, caller = caller_str)
+      deallocate(good_list)
+    else
+      call mpp_error(FATAL, trim(error_header) // ' Empty "' // trim(name) // '" list')
+    endif
+
+    return
+  end function aof_set_coupler_flux_r8
 
   !> \brief Initialize gas flux structures
   !
