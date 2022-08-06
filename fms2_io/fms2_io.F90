395122b9980210bcd8ac2f7269872f854ea7a21d
Bin.Li <bin.li@noaa.gov>
2022-08-06 18:20:05 +0000

    modify fms2_io.F90

--- fms2_io/fms2_io.F90
+++ fms2_io/fms2_io.F90
@@ -372,11 +372,12 @@ subroutine fms2_io_init ()
   READ (input_nml_file, NML=fms2_io_nml, IOSTAT=mystat)
 !>Send the namelist variables to their respective modules
   if (ncchksz .le. 0) then
-        call mpp_error(FATAL, "ncchksz in fms2_io_nml must be a positive number.")
+        call mpp_error(FATAL, "ncchksz in fms2_io_nml must be a positive number."                                             )
   endif
   call netcdf_io_init (ncchksz,header_buffer_val,netcdf_default_format)
   if (header_buffer_val .le. 0) then
-        call mpp_error(FATAL, "header_buffer_val in fms2_io_nml must be a positive number.")
+        #call mpp_error(FATAL, "header_buffer_val in fms2_io_nml must be a positive number.")
+        call mpp_error2(FATAL, "header_buffer_val in fms2_io_nml must be a positive number.")
   endif
   call blackboxio_init (ncchksz)
 !> Mark the fms2_io as initialized
